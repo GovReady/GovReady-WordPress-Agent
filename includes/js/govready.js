ad1beadd757eb8c3ac7eaab48fb111327956b881
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-18 14:35:49 -0700

    Adding example endpoints

--- includes/js/govready.js
+++ includes/js/govready.js
@@ -8,7 +8,44 @@
       'endpoint': '/sites/' + govready.siteId
     }, 
     function(response){
-      console.log('Data from the api', response);
+      console.log('/sites/:siteId', response);
+    }
+  );
+
+
+  jQuery.get(
+
+    ajaxurl, 
+    {
+      'action': 'govready_proxy',
+      'endpoint': '/sites/' + govready.siteId + '/plugins'
+    }, 
+    function(response){
+      console.log('/sites/:siteId/plugins', response);
+    }
+  );
+
+
+  jQuery.get(
+    ajaxurl, 
+    {
+      'action': 'govready_proxy',
+      'endpoint': '/sites/' + govready.siteId + '/accounts'
+    }, 
+    function(response){
+      console.log('/sites/:siteId/accounts', response);
+    }
+  );
+
+
+  jQuery.get(
+    ajaxurl, 
+    {
+      'action': 'govready_proxy',
+      'endpoint': '/sites/' + govready.siteId + '/stack'
+    }, 
+    function(response){
+      console.log('/sites/:siteId/stack', response);
     }
   );
 })(jQuery);
