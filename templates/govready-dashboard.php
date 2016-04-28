53f01a36c78c84f9f44601aa5a2f1f055b633a26
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-28 00:07:34 -0700

    Branding header

--- templates/govready-dashboard.php
+++ templates/govready-dashboard.php
@@ -1,4 +1,7 @@
-<p>
-  This is the dashboard!
-</p>
+<div class="container"><div class="col-xs-12">
+  <h1 style="line-height: 65px;margin: 0;">
+    <img src="<?php print $logo ?>" style="height:50px;float:left;margin-right:10px;" alt="GovReady" />
+    Dashboard
+  </h1>
+</div>
 <div id="root" style="height: 100%"></div>
