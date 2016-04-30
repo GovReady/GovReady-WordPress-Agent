b5d36426e104c6e5d077f93154870d1ce52468b0
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-29 19:03:02 -0700

    Making BETA designations more obvious

--- templates/govready-connect.php
+++ templates/govready-connect.php
@@ -1,7 +1,7 @@
 <div class="container"><div class="col-xs-12">
   <h1 style="line-height: 65px;margin: 10px 0;">
     <img src="<?php print $logo ?>" style="height:50px;float:left;margin-right:10px;" alt="GovReady" />
-    Welcome to GovReady
+    Welcome to GovReady <sup>BETA</sup>
   </h1>
 
   <div class="content">
